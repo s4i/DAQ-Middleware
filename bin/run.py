01c2faf84524f4cf22085ed214b02357a2e85ef4
s4i <b114078@cc.it-hiroshima.ac.jp>
2017-12-15 00:55:15 +0900

    12/15

--- bin/run.py
+++ bin/run.py
@@ -204,7 +204,7 @@ def find_all_files(directory, target_file):
 def file_clean(path):
     file_path = []
     target = 'Makefile'
-    cmd = "rm -r omninames-*.bak omninames-*.log \
+    command = "rm -r omninames-*.bak omninames-*.log \
              omninames-*.ckp rtc.conf .confFilePath __pycache__ .pyc"
     flag = False
     current = os.getcwd()
@@ -232,7 +232,7 @@ def file_clean(path):
         while True:
             choice = raw_input('[y/N]: ').lower()
             if choice in ['y', 'ye', 'yes', '']:
-                subprocess.call(cmd)
+                subprocess.call(command, shell=True)
                 print('Finish')
                 break
             elif choice in ['n', 'no']:
@@ -271,7 +271,7 @@ def file_clean(path):
                         while True:
                             choice = raw_input('[y/N]: ').lower()
                             if choice in ['y', 'ye', 'yes', '']:
-                                subprocess.call(cmd)
+                                subprocess.call(command, shell=True)
                                 break
                             elif choice in ['n', 'no']:
                                 break
